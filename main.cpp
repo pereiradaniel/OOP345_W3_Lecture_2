ec81868146721f3e667f74ded6c4fd48f21876c5
pereiradaniel <danielpaulpereira@protonmail.com>
2021-01-27 12:01:25 -0500

    Commit with comments.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ using namespace std;
 	Ad-hoc polymorphism
 		- overloading
 		- coercion
+
+	True polymorphism
+		- inclusion polymorphism (hierarchy of classes + virtual functions).
 */
 
 int main()
