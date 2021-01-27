6d3382ef0426ad71172382ef5e421d27fee95e1d
pereiradaniel <danielpaulpereira@protonmail.com>
2021-01-27 12:09:11 -0500

    main.cpp returns typeid

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <typeinfo>		// RTTI
 using namespace std;
 
 /*
@@ -8,9 +9,13 @@ using namespace std;
 
 	True polymorphism
 		- inclusion polymorphism (hierarchy of classes + virtual functions).
+		- generic polymorphism / parametric polymorphism (templates).
 */
+
+// RTTI - Run-Time Type Information
 
 int main()
 {
-
+	auto val = 32.1;
+	cout << typeid(val).name();	// Returns a structure, custom type.
 }
